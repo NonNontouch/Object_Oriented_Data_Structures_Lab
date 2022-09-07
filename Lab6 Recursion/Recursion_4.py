69b76a643b3e445eb9df1a959eb51e9647bd4f37
Non <nontouch16974@gmail.com>
2022-09-07 14:38:09 +0700

    Update Recursion_4
    Remove for loop

--- Lab6 Recursion/Recursion_4.py
+++ Lab6 Recursion/Recursion_4.py
@@ -1,5 +1,5 @@
 def perketcalculation(L):
-    if  len(L) == 1:
+    if len(L) == 1:
         s, b = 1, 0
         for j in L[0]:
             tempj = list(map(int, j.split(' ')))
@@ -7,13 +7,8 @@ def perketcalculation(L):
             b += tempj[1]
         return abs(s-b)
     diff = perketcalculation(L[1:])
-    
-    s, b = 1, 0
-    for j in L[0]:
-        tempj = list(map(int, j.split(' ')))
-        s *= tempj[0]
-        b += tempj[1]
-    ismax = abs(s-b)
+    ismax = calculatemorethanone(L[0])
+    ismax = abs(ismax[0]-ismax[1])
     return diff if diff < ismax else ismax
 
 
@@ -24,6 +19,19 @@ def combs(a):
     for c in combs(a[1:]):
         cs += [c, c+[a[0]]]
     return cs
+
+
+def calculatemorethanone(L):
+    if len(L) == 1:
+        temp = list(map(int, L[0].split(' ')))
+        s = temp[0]
+        b = temp[1]
+        return [s, b]
+    result = calculatemorethanone(L[1:])
+    temp = list(map(int, L[0].split(' ')))
+    result[0] *= temp[0]
+    result[1] += temp[1]
+    return result
 
 
 L = input("Enter Input : ").split(',')
