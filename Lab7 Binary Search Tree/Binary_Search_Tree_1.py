21a0c53fd9a1c98447e1a7062150fbcde85b6a07
Non <nontouch16974@gmail.com>
2022-10-11 23:06:23 +0700

    Commit Binary Search Tree #1

---
+++ Lab7 Binary Search Tree/Binary_Search_Tree_1.py
@@ -0,0 +1,80 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        return str(self.data)
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data, root=None):
+        p = self.root
+        if p == None:
+            self.root = Node(data)
+            return self.root
+        else:
+            t = Node(data)
+            while True:
+                if p.data <= data:
+                    if p.right == None:
+                        p.right = t
+                        break
+                    p = p.right
+                else:
+                    if p.left == None:
+                        p.left = t
+                        break
+                    p = p.left    
+            return self.root
+
+    def printTree(self, node, level=0):
+        if node != None:
+            self.printTree(node.right, level + 1)
+            print('     ' * level, node)
+            self.printTree(node.left, level + 1)
+
+
+T = BST()
+inp = [int(i) for i in input('Enter Input : ').split()]
+for i in inp:
+    root = T.insert(i)
+T.printTree(root)
+"""
+Chapter : 7 - item : 1 - รู้จักกับ Binary Search Tree
+ให้น้องรับ input แล้วนำ input นั้นมาสร้าง Binary Search Tree โดย input ตัวแรกสุดจะเป็น Root เสมอ
+
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+    
+    def __str__(self):
+        return str(self.data)
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        # Code Here
+    
+    def printTree(self, node, level = 0):
+        if node != None:
+            self.printTree(node.right, level + 1)
+            print('     ' * level, node)
+            self.printTree(node.left, level + 1)
+
+T = BST()
+inp = [int(i) for i in input('Enter Input : ').split()]
+for i in inp:
+    root = T.insert(i)
+T.printTree(root)
+"""
