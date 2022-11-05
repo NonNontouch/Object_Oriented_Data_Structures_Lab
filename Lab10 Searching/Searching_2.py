f95486541232d6fc891c49b30e253a58ea44100d
Non <nontouch16974@gmail.com>
2022-11-05 20:49:34 +0700

    Commit Searching #2

---
+++ Lab10 Searching/Searching_2.py
@@ -0,0 +1,17 @@
+inp = input("Enter Input : ").split('/')
+lst, num = sorted(list(map(int, inp[0].split(' ')))), list(
+    map(int, inp[1].split(' ')))
+for i in num:
+    try:
+        print(lst[next(x for x, val in enumerate(lst) if val > i)])
+    except:
+        print("No First Greater Value")
+"""
+Chapter : 10 - item : 2 - First Greater Value
+ ส่งมาแล้ว 1 ครั้ง
+ให้น้องเขียนโปรแกรมหาค่าที่น้อยที่สุดที่มากกว่าค่าที่ต้องการจะหา ถ้าหากไม่มีให้แสดงว่า No First Greater Value โดยตัวเลขของทั้ง 2 list รับประกันว่าไม่เกิน 1000000
+Left : [3, 2, 7, 6, 8]         Right : [5, 6, 12]
+1. หาค่าที่น้อยที่สุดที่มากกว่า 5 จาก list (Left) จะได้เป็น 6
+2. หาค่าที่น้อยที่สุดที่มากกว่า 6 จาก list (Left) จะได้เป็น 7
+3. หาค่าที่น้อยที่สุดที่มากกว่า 12 จาก list (Left) จะเห็นว่าไม่มีค่าที่มากกว่า 12 จะแสดงเป็น No First Greater Value
+"""
